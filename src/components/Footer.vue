55a7a843c60a4c1cfe51b119f10bc688d4a53398
Rafli Ramadhan <raflyr44@gmail.com>
2020-12-14 20:06:38 +0700

    aplikasi foodestrian

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    <div class="mt-5 mb-5">
+      <hr />
+      <div class="container">
+        <div class="row">
+          <div class="col text-center">Copyright <i class="far fa-copyright"></i> By Rafli Ramadhan</div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,52 @@
+<template>
+  <!-- Desktop -->
+  <div class="hero">
+    <div class="d-none d-md-block">
+      <div class="row mt-4">
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2><strong>Delicious Food Menu,</strong><br />in Your Gadget</h2>
+              <p>Ayo segera pilih dan pesan makanan favorit anda!</p>
+              <router-link class="btn btn-lg btn-success" to="/foods">
+                <b-icon-arrow-right></b-icon-arrow-right> Pesan
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <img src="../assets/images/hero.png" width="100%" alt="" />
+        </div>
+      </div>
+    </div>
+
+    <!-- Mobile -->
+    <div class="d-sm-block d-md-none">
+      <div class="row mt-4">
+        <div class="col">
+          <img src="../assets/images/hero.png" width="100%" alt="" />
+        </div>
+        <div class="col-md-6 mb-3">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2><strong>Delicious Food Menu,</strong><br />in Your Gadget</h2>
+              <p>Ayos segera pilih dan pesan makanan favorit anda!</p>
+              <button class="btn btn-lg btn-success">
+                <b-icon-arrow-right></b-icon-arrow-right> Pesan
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style>
+</style>
